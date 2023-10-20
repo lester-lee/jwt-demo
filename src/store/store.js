d62833aa45c925e4053a3bcd560e67069dbf6e8e
Lester Lee <contact@lester-lee.com>
2023-10-20 16:04:51 -0400

    Provide store to app

--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,8 @@ const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export default store;
